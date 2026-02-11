6a6727f12eff6451d5fe927ced35706c8d6867e0
Нурдаулет Ризабеков <nurdauletrizabekov@MacBook-Air-Nurdaulet-2.local>
2026-02-11 18:12:53 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ Practice3/classes/init_method.py
@@ -0,0 +1,53 @@
+#Create a class named Person, use the __init__() method to assign values for name and age:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+
+print(p1.name)
+print(p1.age)
+
+#set initial values when creating the object:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Linus", 28)
+p2 = Person("Maks", 18)
+p3 = Person("Rishat", 17)
+print(p1.name, end = ": ")
+print(p1.age)
+print(p2.name, end = ": ")
+print(p2.age)
+print(p3.name, end = ": ")
+print(p3.age)
+
+#Set a default value for the age parameter:
+class Person:
+  def __init__(self, name, age=18):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil")
+p2 = Person("Tobias", 25)
+
+print(p1.name, p1.age)
+print(p2.name, p2.age)
+
+#Create a Person class with multiple parameters:
+class Person:
+  def __init__(self, name, age, city, country):
+    self.name = name
+    self.age = age
+    self.city = city
+    self.country = country
+
+p1 = Person("Linus", 30, "Oslo", "Norway")
+
+print(p1.name)
+print(p1.age)
+print(p1.city)
+print(p1.country)

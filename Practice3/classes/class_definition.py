21a55ff9c7e7d9e1986766d35b8d8c84046cbdbd
Нурдаулет Ризабеков <nurdauletrizabekov@MacBook-Air-Nurdaulet-2.local>
2026-02-11 18:00:23 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ Practice3/classes/class_definition.py
@@ -0,0 +1,25 @@
+#Create a class named MyClass, with a property named x:
+class MyClass:
+  x = 5
+
+#Create an object named p1, and print the value of x:
+p1 = MyClass()
+print(p1.x)
+
+#Delete the p1 object:
+p1 = MyClass()
+print(p1.x)
+del p1
+
+#Create three objects from the MyClass class:
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
+
+#The pass Statement:
+class Person:
+  pass
